0ed22434c95d9c8a119cc6924c1c61f698c2c309
Venkatesh <s.venkatesh779@gmail.com>
2018-02-14 19:02:33 -0800

    changes

--- two_robot_sim.py
+++ two_robot_sim.py
@@ -4,8 +4,41 @@ from scipy.stats import norm
 
 #initialize distribution
 
-dist = np.full((100, 100), 1/(100*100))
+distr = np.full((100, 100), 1/(100*100))
+print len(distr[0:])
 
+def motion_sides():
+	position = [][]
+	for i in range(len(distr[0:])):
+		if i % 2 == 0:  # Moving right
+			for j in range(len(distr[:0])):
+				position = (distr[i+1][j + 1])
+				# Finding target and informing
+				if position[i][j] == distr[30][30]:
+					print "Information Found"
+					return i, j
+				#Finding sub target and taking another action
+				elif position[i][j] == distr[50][60]:
+					print "Taking another route"
+		else:   # Moving left
+			for j in range(len(distr[0:])):
+				position = (distr[i+1][j-1])
+				if position[i][j] == distr[30][30]:
+					print "New Information found"
+					return i,j
+			#plt.plot(position, 'ro')
+			#plt.show()
+
+# Belief update step/ function
+def update():
+	#belief of x is prob(x|target)*bel(x-1). So, prob(x|target) is generated using bayes rule
+	prob_x_t = prob_t_x * prob_x / prob_t
+
+	belief_x = prob[][]
+	prob[i+1][j] = prob[i+1][j] + prob_old / (100*100 - 1)
+
+
+'''
 #initialize robot in the world.. TODO randomly
 world = np.zeros((100,100))
 robot1 = world[0][0]
@@ -17,10 +50,11 @@ for i in range(world[0:][0]):
 		robot2 = robot2 + world[i][j]
 #place the target and surrogates. Function written because we are manually placing the targets now
 def target_position(a,b):
-    target = world[a][b]
-    surr_target1 = world[a-20][b-20]
-    surr_target2 = world[a+20][b+20]
-    return target, surr_target1, surr_target2
+	target = world[a][b]
+	surr_target1 = world[a-20][b-20]
+	surr_target2 = world[a+20][b+20]
+	return target, surr_target1, surr_target2
+
 #define target so that it can be iterated over in the pdf
 def target():
 	probability around target  is high > 1/100*100
@@ -37,3 +71,6 @@ scipy.stats.entropy(dist)
 #Update probabilities 
 print target(50,50)
 
+'''
+if __name__ == '__main__':
+	motion()
